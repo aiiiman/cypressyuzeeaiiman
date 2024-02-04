1bf22599a7c3d247bf74df0f8fb2bf06ef7fe243
Muhammad Aiman <muhammadaiman9943@gmail.com>
2024-02-04 17:29:42 +0800

    Updated with changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+//Import necessary libraries
+const faker = require('faker');
+
+Function to generate random email 
+const generateRandomEmail = () => {
+  return faker.internet.email();
+}
+
+
+describe('Yuzee Sign Up, () => {
+  it('should sign up with random email and validate', () => {
+    cy.visit('https://dev.yuzzee.click')
   })
 })
